a286572854ff0e6816b21c9b75f4822ee9bae7b3
PierreV23 <77776198+PierreV23@users.noreply.github.com>
2024-04-23 15:05:46 +0200

    add api subfolder and ping endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use axum::{routing::get, Router};
+pub mod api;
+use crate::api::ping;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/api/ping", get(ping::ping));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
